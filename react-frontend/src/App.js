0512ba80855704dac0fa33362aef11833b88c7a9
Sam Easton <sam.easton94@gmail.com>
2022-04-21 12:37:23 +0100

    Modified Controller to get current datetime

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,33 +5,21 @@ export default class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { forecasts: [], loading: true };
+        this.state = { currentDateTime: null, loading: true };
     }
 
     componentDidMount() {
-        this.populateWeatherData();
+        this.getTime();
     }
 
-    static renderForecastsTable(forecasts) {
+    static renderDateTime(currentDateTime) {
         return (
             <table className='table table-striped' aria-labelledby="tabelLabel">
                 <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
+                    <tr><th>Date Time</th></tr>
                 </thead>
                 <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
+                        <tr>{currentDateTime.date}</tr>
                 </tbody>
             </table>
         );
@@ -40,20 +28,21 @@ export default class App extends Component {
     render() {
         let contents = this.state.loading
             ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
+            : App.renderDateTime(this.state.currentDateTime);
 
         return (
             <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
+                <h1 id="tabelLabel" >This is the current time</h1>
                 <p>This component demonstrates fetching data from the server.</p>
                 {contents}
             </div>
         );
     }
 
-    async populateWeatherData() {
+    async getTime() {
         const response = await fetch('weatherforecast');
         const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
+        this.setState({ currentDateTime: data, loading: false });
     }
+
 }
